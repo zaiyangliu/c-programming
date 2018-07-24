21e64e48aa10b1456ebb49f59b9fc1983c210338
Coursera Learner <nobody@nowhere.nul>
2018-07-24 08:48:49 +0000

    a

--- 04_compile/hello.c
+++ 04_compile/hello.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define 0 EXIT_SUCCESS
-void main(){
-  printf("hello world\n")
-    return EXIT_SUCCESS;
+int main(void){
+  printf("hello world\n");
+  return EXIT_SUCCESS;
 }
